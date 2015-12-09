f10fbc3caac765c2295bf1d207e373e207f91d74
Norell <norelltagle@yahoo.com>
2015-12-08 19:29:54 -0800

    Skeleton for FileStructureTable

--- src/FileTableEntry.java
+++ src/FileTableEntry.java
@@ -1,4 +1,20 @@
 
 public class FileTableEntry {
+    public int seekPtr;
+    public final Inode inode;
+    public final short iNumber;
+    public int count;
+    public final String mode;
+
+    public FileTableEntry(Inode i, short inumber, String m) {
+        seekPtr = 0;
+        inode = i;
+        iNumber = inumber;
+        count = 1;
+        mode = m;
+        if (mode.compareTo("a") == 0) {
+            seekPtr = inode.length;
+        }
+    }
 
 }
