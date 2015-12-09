9197402513ac330962ed096fc43a4423c33f1cf6
Norell <norelltagle@yahoo.com>
2015-12-08 17:43:44 -0800

    work on directory

--- src/Directory.java
+++ src/Directory.java
@@ -1,4 +1,27 @@
 
 public class Directory {
+    private static int maxChars = 30;
+
+    private int fsize[];
+    private char fnames[][];
+
+    public Directory(int maxINumber) {
+        fsize = new int[maxINumber];
+        for (int i = 0; i < maxINumber; i++) {
+            fsize[i] = 0;
+        }
+        fnames = new char[maxINumber][maxChars];
+        String root = "/";
+        fsize[0] = root.length();
+        root.getChars(0, fsize[0], fnames[0], 0);
+    }
+
+    public int bytes2directory(byte data[]) {
+        
+    }
+
+    public byte[] directory2bytes() {
+
+    }
 
 }
